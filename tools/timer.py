39ddbebc021a96618788cdbf3ea346e3eff57471
Roman Khapov <r.khapov@yandex.ru>
2018-05-18 11:45:56 +0500

    timer

--- tools/timer.py
+++ tools/timer.py
@@ -8,9 +8,7 @@ class Timer:
     def __init__(self, interval=1.0 / 60):
         self._tick = []
         self._interval = interval
-        self._thread = threading.Thread(target=self._tick_event, daemon=True)
-
-        self._in_handing = False
+        self._thread = threading.Thread(target=self._thread_target, daemon=True)
 
     def start(self):
         if not self._thread.is_alive():
@@ -19,17 +17,12 @@ class Timer:
     def add_handler(self, handler):
         self._tick.append(handler)
 
-    def _tick_event(self):
+    def _thread_target(self):
         while True:
             time.sleep(self._interval)
 
-            if self._in_handing:
-                continue
-
-            self._in_handing = True
             for tick in self._tick:
                 tick()
-            self._in_handing = False
 
     def set_interval(self, interval):
         self._interval = interval
